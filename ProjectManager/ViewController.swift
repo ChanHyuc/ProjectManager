fd7225b0b0482449c8b8dec8a36cd82195d33b60
ChanHyuc <whk1140@gmail.com>
2024-11-01 22:19:52 +0900

    Init: Initialize ProjectManager

---
+++ ProjectManager/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ProjectManager
+//
+//  Created by ChanHyuc on 11/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
